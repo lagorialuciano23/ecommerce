91a0aba206a69b35b348a20ad07b06f722aa8f3f
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-11-04 00:25:34 -0300

    Modified: Cards in home

--- src/modules/shared/pages/DashboardHome.jsx
+++ src/modules/shared/pages/DashboardHome.jsx
@@ -2,9 +2,9 @@
 export default function DashboardHome() {
   return (
     <div className="grid grid-rows-2 lg:grid-rows-3 gap-6">
-      <div className="bg-gray-800 p-6 text-white rounded-xl shadow">Card 1</div>
-      <div className="bg-gray-800 p-6 text-white rounded-xl shadow">Card 2</div>
-      <div className="bg-gray-800 p-6 text-white rounded-xl shadow">Card 3</div>
+      <div className="bg-blue-200 p-6 text-blue-900 rounded-xl shadow">Productos actuales: 0</div>
+      <div className="bg-green-200 p-6 text-green-900 rounded-xl shadow">Ordenes actuales: 0</div>
+      <div className="bg-orange-200 p-6 text-orange-900 rounded-xl shadow">Usuarios actuales: 0</div>
     </div>
   );
 }
